905b7329741399db9a61bc266fb153e65aa20ce4
SG1515 <sunggu507@naver.com>
2026-09-17 12:28:37 +0900

    [인프런] 1183

--- coding_test_inflearn/src/coding_test/src/Main.java
+++ coding_test_inflearn/src/coding_test/src/Main.java
@@ -2,34 +2,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
-import java.util.HashMap;
+import java.util.*;
 import java.util.ArrayList;
 
 public class Main {
-	static int n;
-	static int[] ch;
-
-	public void DFS(int L) {
-		if(L == n+1) {
-			String tmp = "";
-			for(int i=1; i<=n; i++) {
-				if(ch[i] == 1) tmp += (i+" ");
+	int answer = 0;
+	int[] dis = {1, -1, 5};
+	int[] ch;
+	
+	Queue<Integer> Q = new LinkedList<>();
+	public int BFS(int s, int e) {
+		ch = new int[10001];
+		ch[s]=1;
+		Q.offer(s);
+		int L = 0;
+		while(!Q.isEmpty()) {
+			int len = Q.size();
+			
+			for(int i=0; i<len; i++) {
+				int x = Q.poll();
+				for(int j=0; j<dis.length; j++) {
+					int nx = x + dis[j];
+					if(nx == e) return L+1;
+					if(nx >= 1 && nx <= 10000 && ch[nx]==0) {
+						ch[nx]=1;
+						Q.offer(nx);
+					}
+				}
+				
 			}
-			if(tmp.length()>0) System.out.println(tmp);
+			L++;
 		}
-		else {
-			ch[L] = 1;
-			DFS(L+1);
-			ch[L] = 0;
-			DFS(L+1); 
-		}
+		return 0;
 	}
+	
 	
 	public static void main(String[] args) throws IOException{
 		Main T = new Main();
-		n = 3;
-		ch = new int[n+1];
-		T.DFS(1);
+		Scanner kb = new Scanner(System.in);
+		int s = kb.nextInt();
+		int e = kb.nextInt();
+		System.out.println(T.BFS(s, e));
 	}
 	
 	
