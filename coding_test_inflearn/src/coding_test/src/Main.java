5b6c449a80cfdb42b135976a5e12c00de2fc80e6
SG1515 <sunggu507@naver.com>
2026-09-18 21:44:26 +0900

    [인프런] 위치 찾기-DFS

--- coding_test_inflearn/src/coding_test/src/Main.java
+++ coding_test_inflearn/src/coding_test/src/Main.java
@@ -6,43 +6,33 @@ import java.util.*;
 import java.util.ArrayList;
 
 public class Main {
-	int answer = 0;
-	int[] dis = {1, -1, 5};
-	int[] ch;
+	static int answer = Integer.MIN_VALUE, c, n;	
 	
-	Queue<Integer> Q = new LinkedList<>();
-	public int BFS(int s, int e) {
-		ch = new int[10001];
-		ch[s]=1;
-		Q.offer(s);
-		int L = 0;
-		while(!Q.isEmpty()) {
-			int len = Q.size();
-			
-			for(int i=0; i<len; i++) {
-				int x = Q.poll();
-				for(int j=0; j<dis.length; j++) {
-					int nx = x + dis[j];
-					if(nx == e) return L+1;
-					if(nx >= 1 && nx <= 10000 && ch[nx]==0) {
-						ch[nx]=1;
-						Q.offer(nx);
-					}
-				}
-				
-			}
-			L++;
+	public void DFS(int L, int sum, int[] arr) {
+		if(sum > c) return;
+		if(L == n) {
+			answer = Math.max(answer, sum);
+		} else {
+			DFS(L+1, sum+arr[L], arr);
+			DFS(L+1, sum, arr);
 		}
-		return 0;
 	}
 	
 	
 	public static void main(String[] args) throws IOException{
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		int s = kb.nextInt();
-		int e = kb.nextInt();
-		System.out.println(T.BFS(s, e));
+		c = kb.nextInt();
+		n = kb.nextInt();
+		int arr[] = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		T.DFS(0, 0, arr);
+		
+		System.out.println(answer);
+		
 	}
 	
 	
